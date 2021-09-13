476ad531f13f98238b6047bb91ae01fa9fff5700
Gabriel Henrique Linke <gh.linke@hotmail.com>
2021-09-13 01:13:32 -0300

    Create DownloadMusic class and separate interface from the class

---
+++ DownloadMusic.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const ytfps = require('ytfps');
+
+class DownloadMusic {   
+    constructor(baseDirectory, maxDownloads) {
+        this.MAX_SIMULTANEOUS_DOWNLOADS = maxDownloads;
+        this.baseDirectory = baseDirectory;
+        this.currentlyDownloading = 0;
+    }
+
+    logError(str)
+    {
+        console.log(str);
+        fs.appendFileSync(this.baseDirectory+'error-log-file.txt', str, "UTF-8",{'flags': 'a'});
+    }
+
+    log(str)
+    {
+        console.log(str);
+        fs.appendFileSync(this.baseDirectory+'log-file.txt', str, "UTF-8",{'flags': 'a'});
+    }
+
+    async getTitle(url)
+    {
+        let info = await ytdl.getInfo(url);
+        return info.videoDetails.title;
+    }
+    
+    download(url)
+    {
+        const $this = this;
+        const stream = ytdl(url);
+        let proc = new ffmpeg({source:stream});
+        this.getTitle(url)
+        .then(res => {
+            $this.log(`Downloading ${res}...\n`);
+            const validFilename = res.replace(/[/\\?%*:|"<>]/g, '');
+            proc.setFfmpegPath('/usr/bin/ffmpeg');
+            proc.withAudioCodec('libmp3lame')
+                    .toFormat('mp3')
+                    .output($this.baseDirectory + validFilename + '.mp3')
+                    .on('error', function(err) {
+                        $this.logError(`An error occurred: ${err.message}\n`);
+                        $this.currentlyDownloading--;
+                    })
+                    .on('end', function() {
+                        $this.log(`Processing finished! Downloaded ${res} from ${url}\n`);
+                        $this.currentlyDownloading--;
+                    })
+                    .run()
+        })
+        .catch(err => {
+            $this.logError(`\n${err}\n\n`);
+            $this.logError(`Couldn't download audio from ${url}\n`);
+            $this.currentlyDownloading--;
+        });
+    }
+    
+    async getPlaylistVideos(url)
+    {
+        const intermediateUrl = url.split('list=');
+        const requestUrl = intermediateUrl[1].split('&')[0];
+        const playlist = await ytfps(requestUrl);
+        let videos = playlist.videos;
+        return videos.map(video => video.url);
+    }
+    
+    async downloadAllPlaylistVideos(playlistUrl)
+    {
+        let videosQueue = await this.getPlaylistVideos(playlistUrl);
+        this.log(`\nStarting to download ${videosQueue.length} videos from playlist: ${playlistUrl}\n`);
+        const updateDownloadsQueue = setInterval(() => {
+            if(videosQueue.length === 0) clearInterval(updateDownloadsQueue);
+            while(this.currentlyDownloading < this.MAX_SIMULTANEOUS_DOWNLOADS && videosQueue.length > 0)
+            {
+                this.currentlyDownloading++;
+                this.download(videosQueue.shift());
+            }
+        }, 500);
+    }
+}
+
+module.exports = { DownloadMusic }
--- main.js
+++ main.js
@@ -1,83 +1,14 @@
 const fs = require('fs');
-const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
-const ytfps = require('ytfps');
 const readline = require("readline");
- 
-const MAX_SIMULTANEOUS_DOWNLOADS = 4;
+const { DownloadMusic } = require('./DownloadMusic');
 let baseDirectory = '../../Gabriel/Músicas/';
-
-let videosQueue = [];
-let currentlyDownloading = 0;
-
-async function getTitle(url)
-{
-    let info = await ytdl.getInfo(url);
-    return info.videoDetails.title;
-}
-
-function download(url)
-{
-    const stream = ytdl(url)
-    let proc = new ffmpeg({source:stream});
-    getTitle(url)
-    .then(res => {
-        log(`Downloading ${res}...\n`);
-        const validFilename = res.replace(/[/\\?%*:|"<>]/g, '');
-        proc.setFfmpegPath('/usr/bin/ffmpeg');
-        proc.withAudioCodec('libmp3lame')
-                .toFormat('mp3')
-                .output(baseDirectory + validFilename + '.mp3')
-                .on('error', function(err) {
-                    log(`An error occurred: ${err.message}\n`);
-                    currentlyDownloading--;
-                })
-                .on('end', function() {
-                    log(`Processing finished! Downloaded ${res} from ${url}\n`);
-                    currentlyDownloading--;
-                })
-                .run()
-    })
-    .catch(err => {
-        log(`\n${err}\n\n`);
-        log(`Couldn't download audio from ${url}\n`);
-        currentlyDownloading--;
-    });
-}
-
-async function getPlaylistVideos(url)
-{
-    const intermediateUrl = url.split('list=');
-    const requestUrl = intermediateUrl[1].split('&')[0];
-    const playlist = await ytfps(requestUrl);
-    let videos = playlist.videos;
-    return videos.map(video => video.url);
-}
-
-async function downloadAllPlaylistVideos(playlistUrl)
-{
-    videosQueue = await getPlaylistVideos(playlistUrl);
-    log(`\nStarting to download ${videosQueue.length} videos from playlist: ${playlistUrl}\n`);
-    let updateDownloadsQueue = setInterval(function() {
-        if(videosQueue.length === 0) clearInterval(updateDownloadsQueue);
-        while(currentlyDownloading < MAX_SIMULTANEOUS_DOWNLOADS && videosQueue.length > 0)
-        {
-            currentlyDownloading++;
-            download(videosQueue.shift());
-        }
-    }, 500);
-}
-
-function log(str)
-{
-    console.log(str);
-    fs.appendFileSync(baseDirectory+'log-file.txt', str, "UTF-8",{'flags': 'a'});
-}
+let maxDownloads = 4;
+let Backend;
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    input: process.stdin,
+    output: process.stdout,
+  });
 
 rl.question("Qual a pasta de destino dos arquivos?\n", function (answer) {
     if(answer != '')
@@ -85,18 +16,20 @@ rl.question("Qual a pasta de destino dos arquivos?\n", function (answer) {
         baseDirectory = baseDirectory + answer + '/';
         if (!fs.existsSync(baseDirectory)) fs.mkdirSync(baseDirectory, { recursive: true });
     }
+    Backend = new DownloadMusic(baseDirectory, maxDownloads);
+    
     rl.question("Deseja realizar a conversão de um vídeo ou de uma playlist?\n1: Vídeo\n2: Playlist\n\n", function (answer) {
         if(answer == 1) {
             console.log(`\nVocê escolheu vídeo. Qual o link do vídeo?\n`);
             rl.question("URL: ", function (answer) {
-                download(answer);
+                Backend.download(answer);
                 rl.close();
             });
         }
         else if(answer == 2) {
             console.log(`Você escolheu playlist. Qual o link da playlist?\n`);
             rl.question("URL: ", function (answer) {
-                downloadAllPlaylistVideos(answer);
+                Backend.downloadAllPlaylistVideos(answer);
                 rl.close();
             });
         }
